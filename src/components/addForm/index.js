1045a2abb8942efe2a10fd0bfabac3716feb1126
GFZ\shw5691 <dustmzmfla@naver.com>
2024-05-31 11:03:41 +0900

    datepicker 추가

--- src/components/addForm/index.js
+++ src/components/addForm/index.js
@@ -6,11 +6,30 @@ const StyleCss = styled.div`
 `;
 
 const AddForm = (props) => {
+
     // 헤더 타이틀 변경
     useEffect(()=>{ props.headerTitle('일정추가'); })
 
     return (
-        <StyleCss></StyleCss>
+        <StyleCss>
+            <form method="post">
+                <div className="formContents">
+                    <label>여행지</label>
+                    <input type="text" placeholder="여행지 입력"></input>
+                </div>
+
+                <div className="formContents">
+                    <label>여행제목</label>
+                    <input type="text" placeholder="미입력시 여행지+여행으로 고정"></input>
+                </div>
+
+                <div className="formContents">
+                    <label>여행기간</label>
+
+                    
+                </div>
+            </form>
+        </StyleCss>
     )
 }
 
